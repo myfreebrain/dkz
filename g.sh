069fc6b1a7dee90dc92f73aeeb05ca1dff61a834
myfreebrain <freebrain@qq.com>
2022-12-01 23:18:00 +0800

    first commit

---
+++ g.sh
@@ -0,0 +1,9 @@
+echo "# dklocales" >> README.md
+git init
+git add -A
+
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/myfreebrain/dkz.git
+git push -u origin main
+
